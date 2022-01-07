203e3ffaee89924d43de025a5c94d28ddee5047d
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2022-01-07 17:17:25 -0300

    adicionado contador

--- src/components/alunos/Alunos.tsx
+++ src/components/alunos/Alunos.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { alunosData } from "./alunoData";
 import AlunoList from "./AlunoList";
 
 const Alunos: React.FC = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("Entrou no useEffect");
+
+    if (counter !== 10) return;
+
+    alert("O contador chegou no número 10");
+  }, [counter]);
+
   return (
     <>
       <AlunoList alunos={alunosData} />
+
+      <h1>{counter}</h1>
+
+      <button onClick={() => setCounter(counter + 1)}>
+        Incrementar contador
+      </button>
     </>
   );
 };
